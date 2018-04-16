9c22cadd6849f93fdf726d4cb656c43764f7d82c
dannyb21892 <dannyb21892@verizon.net>
2018-04-16 13:17:45 -0400

    added backend

---
+++ Back_end/db/migrate/20180416170506_create_games.rb
@@ -0,0 +1,10 @@
+class CreateGames < ActiveRecord::Migration[5.1]
+  def change
+    create_table :games do |t|
+      t.integer :player_id
+      t.integer :score
+      t.integer :duration
+      t.timestamps
+    end
+  end
+end
